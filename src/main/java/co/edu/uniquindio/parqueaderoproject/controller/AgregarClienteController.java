2e163a18ae57f559dedf4d41fbb35764d2bdd339
Camila <angieec.gutierrezt@uqvirtual.edu.co>
2025-05-28 13:06:25 -0500

    Agregar cliente y agregar vehiculo ventanas contenido

--- src/main/java/co/edu/uniquindio/parqueaderoproject/controller/AgregarClienteController.java
+++ src/main/java/co/edu/uniquindio/parqueaderoproject/controller/AgregarClienteController.java
@@ -1,5 +1,26 @@
 package co.edu.uniquindio.parqueaderoproject.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class AgregarClienteController {
+
+    @FXML
+    private TextField txtCedulaCliente;
+
+    @FXML
+    private TextField txtCorreoCliente;
+
+    @FXML
+    private TextField txtNombreCliente;
+
+    @FXML
+    private TextField txtTelefonoCliente;
+
+    @FXML
+    void guardarCliente(ActionEvent event) {
+
+    }
 
 }
--- src/main/java/co/edu/uniquindio/parqueaderoproject/controller/AgregarVehiculoController.java
+++ src/main/java/co/edu/uniquindio/parqueaderoproject/controller/AgregarVehiculoController.java
@@ -1,4 +1,43 @@
 package co.edu.uniquindio.parqueaderoproject.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class AgregarVehiculoController {
+
+    @FXML
+    private ComboBox<?> cmbTipoMembresia;
+
+    @FXML
+    private ComboBox<?> cmbTipoVehiculo;
+
+    @FXML
+    private TextField txtColor;
+
+    @FXML
+    private TextField txtDatoAdicional;
+
+    @FXML
+    private TextField txtModelo;
+
+    @FXML
+    private TextField txtPlaca;
+
+    @FXML
+    void guardarVehiculo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarMembresia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void seleccionarTipoVehiculo(ActionEvent event) {
+
+    }
+
 }
