07c5e589cc0d817e7412c81962661c4e5eb83ade
Camila <angieec.gutierrezt@uqvirtual.edu.co>
2025-05-28 12:03:14 -0500

    Interfaces services y repositores

---
+++ src/main/java/co/edu/uniquindio/parqueaderoproject/repositories/interfaces/IngresoRepositorie.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.parqueaderoproject.repositories.interfaces;
+
+public interface IngresoRepositorie {
+}
---
+++ src/main/java/co/edu/uniquindio/parqueaderoproject/service/interfaces/ClienteService.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.parqueaderoproject.service.interfaces;
+
+public interface ClienteService {
+}
