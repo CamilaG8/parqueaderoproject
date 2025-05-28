48da6dcdfd8e7be7c05c6815714ccf53ba639ba4
Camila <angieec.gutierrezt@uqvirtual.edu.co>
2025-05-28 12:24:45 -0500

    Administracion controller y vista administracion

--- src/main/java/co/edu/uniquindio/parqueaderoproject/controller/AdministracionController.java
+++ src/main/java/co/edu/uniquindio/parqueaderoproject/controller/AdministracionController.java
@@ -1,4 +1,51 @@
 package co.edu.uniquindio.parqueaderoproject.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class AdministracionController {
+
+    @FXML
+    private TextField txtPuestoCamion;
+
+    @FXML
+    private TextField txtPuestoCarro;
+
+    @FXML
+    private TextField txtPuestoMoto;
+
+    @FXML
+    private TextField txtTarifaAnual;
+
+    @FXML
+    private TextField txtTarifaCamion;
+
+    @FXML
+    private TextField txtTarifaCarro;
+
+    @FXML
+    private TextField txtTarifaMensual;
+
+    @FXML
+    private TextField txtTarifaMoto;
+
+    @FXML
+    private TextField txtTarifaTrimestre;
+
+    @FXML
+    void guardarCambiosMembresia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarCambiosPuestos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void guardarCambiosTarifas(ActionEvent event) {
+
+    }
+
 }
