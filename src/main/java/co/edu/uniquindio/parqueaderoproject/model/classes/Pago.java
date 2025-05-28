562be5824842086000bc499e617dfe06f1720151
VicSanz <mas_alla_vasp@hotmail.com>
2025-05-27 19:20:35 -0500

    Clase Pago implementada con cálculo por hora y validación de membresía

--- src/main/java/co/edu/uniquindio/parqueaderoproject/model/classes/Pago.java
+++ src/main/java/co/edu/uniquindio/parqueaderoproject/model/classes/Pago.java
@@ -1,4 +1,17 @@
 package co.edu.uniquindio.parqueaderoproject.model.classes;
 
+import java.time.temporal.ChronoUnit;
+
 public class Pago {
+
+    public double calcularMonto(Ingreso ingreso, Tarifa tarifa, Vehiculo vehiculo) {
+        if (vehiculo.getMembresia() != null && vehiculo.getMembresia().estaActiva()) {
+        return 0.0;
+        }
+        long horas = ChronoUnit.HOURS.between(ingreso.getEntrada(), ingreso.getSalida());
+        if (horas == 0) {
+            horas = 1;
+        }
+        return tarifa.getValorHora() * horas;
+    }
 }
